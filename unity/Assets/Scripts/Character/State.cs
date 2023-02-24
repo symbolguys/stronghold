174729d27fed285d44b8c27777b55974ff97c40a
Molzen <33835030+Molzen@users.noreply.github.com>
2023-02-24 12:44:08 +0100

    add animation to character

---
+++ unity/Assets/Scripts/Character/State.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Animator>().Play("Idle");
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+
+    void UpdatePosition(Vector3 position)
+    {
+        transform.position = position;
+    }
+
+    void SetAnimationController(string state)
+    {
+        GetComponent<Animator>().Play(state);
+    }
+}
