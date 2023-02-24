b672e3d112983326b1a3ce575cc104f178d08040
Flemming Lundahl <flemming@pop-os.localdomain>
2023-02-24 09:41:20 +0100

    Revert "add jazerant remote"
    This reverts commit a564c5bfc82b257ad4c2331cc4f4e8fd55a5f5ee.

--- frontend/apps/stronghold/src/app/app.routes.ts
+++ frontend/apps/stronghold/src/app/app.routes.ts
@@ -3,10 +3,6 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'jazerant',
-    loadChildren: () => import('jazerant/Routes').then((m) => m.remoteRoutes),
-  },
-  {
     path: '',
     component: NxWelcomeComponent,
   },
--- frontend/apps/stronghold/src/remotes.d.ts
+++
@@ -1,1 +0,0 @@
-declare module 'jazerant/Routes';
